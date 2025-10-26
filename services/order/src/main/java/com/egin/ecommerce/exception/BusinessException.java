09e6dd5f9508ee748183b92834581ca2fe6ba02a
Ahmet Egin <eginahmet62@gmail.com>
2025-10-27 00:56:35 +0300

    completed

--- services/order/src/main/java/com/egin/ecommerce/exception/BusinessException.java
+++ services/order/src/main/java/com/egin/ecommerce/exception/BusinessException.java
@@ -2,10 +2,20 @@ package com.egin.ecommerce.exception;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
 
-@EqualsAndHashCode(callSuper=true)
+@EqualsAndHashCode(callSuper = true)
 @Data
+@Getter
 public class BusinessException extends RuntimeException {
     private final String msg;
 
+    public BusinessException(String msg) {
+        super(msg);
+        this.msg = msg;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
 }
